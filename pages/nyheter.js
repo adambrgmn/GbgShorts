b5ab96a766b33731f3adcf54b041b292f6180d3b
Adam Bergman <adam@fransvilhelm.com>
2017-02-02 21:56:15 +0100

    Add placeholder pages

---
+++ pages/nyheter.js
@@ -0,0 +1,28 @@
+import React, { Component, PropTypes } from 'react';
+
+import Header from '../components/Header';
+import Container from '../components/Container';
+
+export default class Nyheter extends Component {
+  static propTypes = {
+    url: PropTypes.shape({
+      pathname: PropTypes.string,
+    }).isRequired,
+  }
+
+  static async getInitialProps() {
+    await new Promise((resolve) => {
+      setTimeout(resolve, 1000);
+    });
+
+    return {};
+  }
+
+  render() {
+    return (
+      <Container>
+        <Header pathname={this.props.url.pathname} />
+      </Container>
+    );
+  }
+}
