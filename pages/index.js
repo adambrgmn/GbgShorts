9d660c3d8e6f1b7eb05e1558d369304ce000b723
Adam Bergman <adam@fransvilhelm.com>
2017-02-02 11:41:26 +0100

    Update index

--- pages/index.js
+++ pages/index.js
@@ -14,17 +14,30 @@ export default class Index extends Component {
     }).isRequired,
   }
 
+  static async getInitialProps() {
+    await new Promise((resolve) => {
+      setTimeout(resolve, 1000);
+    });
+
+    return {};
+  }
+
+  state = { imgs: [0, 1, null] }
+
+  componentDidMount() {
+    this.addImages();
+  }
+
+  addImages = () => this.setState(({ imgs }) => ({ imgs: [...imgs, 2, 3, 6, 0, 1, 2, 3, 4, 2] }));
   cloudinarySrc = n => `http://res.cloudinary.com/adambrgmn/image/upload/c_lfill,g_face,h_360,w_550/v1485974209/gbgshorts/gbgshorts-${n}.jpg`
 
   render() {
-    const imgs = [0, 1, null, 2, 3, 6];
-
     return (
       <Container bg="#d5c0c0">
         <Header pathname={this.props.url.pathname} />
         <Grid>
-          {imgs.map((n, i) => (
-            <GridColumn key={n == null ? 'text' : n}>
+          {this.state.imgs.map((n, i) => (
+            <GridColumn key={n == null ? 'text' : n + i}>
               {i === 2
                 ? <WelcomeSection />
                 : <Img src={this.cloudinarySrc(n)} />
