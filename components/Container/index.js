d94b9144e845e2a3484e066fb31a387430b08bb6
Adam Bergman <adam@fransvilhelm.com>
2017-02-02 11:40:49 +0100

    Update container component

--- components/Container/index.js
+++ components/Container/index.js
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 
 export default styled.div`
   max-width: 100vw;
-  padding: 0;
+  min-height: 100vh;
+  padding: 0 0 5rem 0;
   margin: 0;
   background-color: ${({ bg }) => bg};
 `;
